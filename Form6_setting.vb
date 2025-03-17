66c66c493ee38ddd0fd418768e82c6482ecaeaaf
naiaru917 <naiaru.krkrgrgr@gmail.com>
2025-03-17 22:27:53 +0900

    Update Form6_setting.vb

--- Form6_setting.vb
+++ Form6_setting.vb
@@ -1,17 +1,24 @@
 ﻿Public Class Form6
     Private Sub Form6_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'フォームの位置を設定
         Me.Top = Form1.Top
         Me.Left = Form1.Left
+
+        'BGMのオンオフ
         If bgm = True Then
             RadioButton1.Checked = True
         Else
             RadioButton2.Checked = True
         End If
+        
+        'フィーバータイムのオンオフ
         If fever = True Then
             RadioButton3.Checked = True
         Else
             RadioButton4.Checked = True
         End If
+
+        '効果音のオンオフ
         If sound = True Then
             RadioButton5.Checked = True
         Else
@@ -19,8 +26,9 @@
         End If
     End Sub
 
+    '確定ボタンをクリックしたとき
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        '確定ボタン
+        '設定項目を記録
         If RadioButton1.Checked = True Then
             bgm = True
         Else
@@ -37,17 +45,20 @@
             sound = False
         End If
 
+        '効果音がオンの場合に再生
         If sound = True Then
             BtnSound()
         End If
 
-        Me.Close()
+        Me.Close()    '設定画面を閉じる
     End Sub
 
+    'BGMをオンにしたタイミングでBGMを再生
     Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
         startBGM1()
     End Sub
 
+    'BGMをオフにしたタイミングでBGMを停止
     Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
         stopBGM()
     End Sub
